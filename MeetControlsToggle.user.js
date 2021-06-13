279e692d979ff2cd0d704d65d607889238b04b2a
Victor <vicholz@gmail.com>
2021-06-12 23:21:36 -0700

    Updates to selectors and config
    1. Updating selectors to match public GA meet version elements.
    2. Adding context menu option in TamperMonkey.

--- MeetControlsToggle.user.js
+++ MeetControlsToggle.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Meet Controls Toggle
-// @version      0.2
-// @description  Use 'ESC' button to hide/show Meet controls for cleaner video when trying to capture in OBS.
+// @version      0.3
+// @description  Use TM context menu or 'ESC' button to hide/show Meet controls for cleaner video when trying to capture in OBS.
 // @author       Victor Holz
 // @downloadURL  https://github.com/vicholz/userscripts/raw/master/MeetControlsToggle.user.js
 // @updateURL    https://github.com/vicholz/userscripts/raw/master/MeetControlsToggle.user.js
 // @match        https://meet.google.com/*
-// @icon         https://www.google.com/s2/favicons?domain=google.com
-// @grant        none
+// @icon         https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v1/web-64dp/logo_meet_2020q4_color_1x_web_64dp.png
+// @run-at       document-end
+// @grant        GM.registerMenuCommand
 // ==/UserScript==
 
 (function() {
@@ -43,28 +44,71 @@
         }
     }
 
+    function toggleControls(){
+        let elements = [
+            //document.querySelector('div[aria-label*="Leave call"]') ? document.querySelector('div[aria-label*="Leave call"]').parentNode.parentNode.parentNode : null,
+            document.querySelector('button[aria-label*="Leave call"]') ? document.querySelector('button[aria-label*="Leave call"]').parentNode.parentNode.parentNode.parentNode.parentNode : null,
+            document.querySelector('div[aria-label*="Show everyone"]') ? document.querySelector('div[aria-label*="Show everyone"]').parentNode.parentNode.parentNode.parentNode : null,
+            //document.querySelectorAll('div[data-self-name*="You"]') ? document.querySelectorAll('div[data-self-name*="You"]')[2].parentNode.parentNode : null,
+            //document.querySelectorAll('div[data-self-name*="You"]') ? document.querySelectorAll('div[data-self-name*="You"]')[0].parentNode : null,
+            document.querySelectorAll('div[data-self-name*="You"]') ? document.querySelectorAll('div[data-self-name*="You"]')[1].parentNode.parentNode : null,
+            document.querySelector('div[data-icon-type*="4"]') ? document.querySelector('div[data-icon-type*="4"]').parentNode.parentNode.parentNode : null,
+            document.querySelector('div[aria-label*="Pin yourself to your main screen."]') ? document.querySelector('div[aria-label*="Pin yourself to your main screen."]').parentNode.parentNode.parentNode.parentNode.parentNode : null,
+            document.evaluate("//i[contains(., 'present_to_all')]", document, null, XPathResult.ANY_TYPE, null).iterateNext()
+        ];
+        console.log("Toggling element(s) visibility...");
+        if (window.elementsVisible){
+            window.elementsVisible = false;
+            hideElements(elements);
+        } else {
+            window.elementsVisible = true;
+            showElements(elements);
+        }
+    }
+
     window.elementsVisible = true;
     let i = setInterval(function(){
+        console.log("Looking for controls....");
         try {
-            let video = document.querySelectorAll("video")[1];
-            //let video = document.querySelector('div[data-layout*="roi-crop"]')
-            document.addEventListener("keyup", function(e){
-                let elements = [
-                    document.querySelector('div[aria-label*="Leave call"]').parentNode.parentNode.parentNode,
-                    document.querySelector('div[aria-label*="Show everyone"]').parentNode.parentNode.parentNode.parentNode,
-                    document.querySelectorAll('div[data-self-name*="You"]')[2].parentNode.parentNode
-                ];
-                if (e.keyCode === 27){
-                    console.log("Toggling element(s) visibility...");
-                    if (window.elementsVisible){
-                        window.elementsVisible = false;
-                        hideElements(elements);
-                    } else {
-                        window.elementsVisible = true;
-                        showElements(elements);
-                    }
+            let foundElement = false;
+            let waitForElements = [
+                document.querySelector('div[aria-label*="Leave call"]'),
+                document.querySelector('button[aria-label*="Leave call"]')
+            ];
+            waitForElements.forEach(function(e){
+                if (e !== undefined && e !== null) {
+                    foundElement = true;
                 }
             });
+            if (!foundElement) {
+                throw "Controls not found! Will try again...";
+            } else {
+                console.log("Found controls.");
+            }
+            console.log("Looking for video element...");
+            let video = document.querySelectorAll("video")[1];
+            if (!video){
+                video = document.querySelectorAll("video")[0];
+            }
+            if (!video){
+                throw "Could not find video element! Will try again...";
+            }
+            console.log("Found video element.");
+
+            console.log("Adding context menu item...");
+            GM.registerMenuCommand("Toggle Meet Controls", toggleControls);
+            
+            console.log("Adding event listener...");
+            window.addEventListener('DOMContentLoaded', function() {
+                document.addEventListener("keypress", function(e) {
+                    console.log("Detected keypress...");
+                    if (e.keyCode === 27){
+                        console.log("Toggling element(s) visibility...");
+                        toggleControls();
+                    }
+                }, false);
+            }, false);
+            
             clearInterval(i);
         } catch(e) {
             console.error("Could not find video element. Will retry...");
