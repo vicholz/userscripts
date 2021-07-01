fd9918ea3132faff05f99152b38914ea97daa9e7
Victor <vicholz@gmail.com>
2021-06-30 23:23:18 -0700

    Adding update and download urls

--- Amazon Smile Redirector.user.js
+++ Amazon Smile Redirector.user.js
@@ -3,6 +3,8 @@
 // @version      0.1
 // @description  Redirects cart and checkout to smile.amazon.com.
 // @author       Victor Holz
+// @downloadURL  https://github.com/vicholz/userscripts/raw/master/Amazon%20Smile%20Redirector.user.js
+// @updateURL    https://github.com/vicholz/userscripts/raw/master/Amazon%20Smile%20Redirector.user.js
 // @match        https://www.amazon.com/gp/cart/view.html*
 // @match        https://www.amazon.com/gp/buy/spc/handlers/display.html*
 // @icon         https://www.google.com/s2/favicons?domain=amazon.com
