32e83f966f3becb06dfdb19023314b68eb94b045
Victor <vicholz@gmail.com>
2021-11-05 17:26:45 -0700

    Update AmazonSmileRedirector.user.js

--- AmazonSmileRedirector.user.js
+++ AmazonSmileRedirector.user.js
@@ -1,22 +1,28 @@
 // ==UserScript==
 // @name         Amazon Smile Redirector
-// @version      0.1
+// @version      0.2
 // @description  Redirects cart and checkout to smile.amazon.com.
 // @author       Victor Holz
-// @downloadURL  https://github.com/vicholz/userscripts/raw/master/Amazon%20Smile%20Redirector.user.js
-// @updateURL    https://github.com/vicholz/userscripts/raw/master/Amazon%20Smile%20Redirector.user.js
-// @match        https://www.amazon.com/gp/cart/view.html*
-// @match        https://www.amazon.com/gp/buy/spc/handlers/display.html*
+// @downloadURL  https://github.com/vicholz/userscripts/raw/master/AmazonSmileRedirector.user.js
+// @updateURL    https://github.com/vicholz/userscripts/raw/master/AmazonSmileRedirector.user.js
+// @match        https://www.amazon.com/*
 // @icon         https://www.google.com/s2/favicons?domain=amazon.com
 // @grant        none
+// @run-at document-start
+// @noframes
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    if (location.hostname === "www.amazon.com"){
-        console.log("Redirecting to https://smile.amazon.com...");
-        let path = location.href.split("https://www.amazon.com/")[1];
-        location.replace(`https://smile.amazon.com/${path}`);
-    }
+    var i = setInterval(function(){
+        if (location.hostname === "www.amazon.com"){
+            if (location.pathname.includes("/gp/buy/spc/handlers/display.html")){
+                console.log("Redirecting to https://smile.amazon.com...");
+                let path = location.href.split("https://www.amazon.com/")[1];
+                location.replace(`https://smile.amazon.com/${path}`);
+                clearInterval(i);
+            }
+        }
+    },500);
 })();
